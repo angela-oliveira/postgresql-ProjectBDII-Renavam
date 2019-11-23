191ce46b8be7c2105504626461aef0253655db5d
andreia23 <44982439+andreia23@users.noreply.github.com>
2019-11-23 15:45:01 -0300

    Update Views.sql

--- Views.sql
+++ Views.sql
@@ -1,11 +1,13 @@
--- Visão 1: tabela que indica a relação de condutores com pontos na carteira
+################# Visão 1: tabela que indica a relação de condutores com pontos na carteira ################# 
+
+                    ---Falta testar
 
 CREATE VIEW condutor_pontosCnh AS
-( SELECT con.idcadastro,con.nome as Condutor, 
-con.idcategoriacnh, date_part('year',mult.datainfracao)
-as ano, sum(infra.pontos)
-FROM condutor con  JOIN multa mult
-ON con.idcadastro = mult.idcondutor  join infracao infra
-on mult.idinfracao = infra.idinfracao
-GROUP BY date_part('year',mult.datainfracao), con.idcadastro
+    ( SELECT con.idcadastro,con.nome as Condutor, 
+    con.idcategoriacnh, date_part('year',mult.datainfracao)
+    as ano, sum(infra.pontos)
+    FROM condutor con  JOIN multa mult
+    ON con.idcadastro = mult.idcondutor  join infracao infra
+    on mult.idinfracao = infra.idinfracao
+    GROUP BY date_part('year',mult.datainfracao), con.idcadastro
 );
